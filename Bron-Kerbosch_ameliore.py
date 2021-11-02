f2bd1b4e9609ba7fd2712fbae0d8f5016618a6c1
WahabBello <abdouwahab123@gmail.com>
2021-11-02 02:17:20 +0100

    Bug fixed in partie 2

--- Bron-Kerbosch_ameliore.py
+++ Bron-Kerbosch_ameliore.py
@@ -1,7 +1,8 @@
 # Trouver toutes les cliques maximales dans un graphe en utilisant l'algorithme de Bron-Kerbosch. Le graphe d'entrée est ici 
 # au format liste d'adjacence, un dict avec des sommets comme clés et des listes de leurs voisins comme valeurs.
 
-from collections import defaultdict
+# from collections import defaultdict
+import copy
 
 def version_ameliore(graphe):
   p = set(graphe.keys())
@@ -26,47 +27,106 @@ def version_ameliore_avec_pivot(graphe, r, p, x, cliques):
       p.remove(v)
       x.add(v)
 
-def Degenerescence(graphe):
-  ordre = []
-  ordre_set = set()
-  degrees = defaultdict(lambda : 0)
-  degen = defaultdict(list)
-  deg_max = -1
 
-  for v in graphe:
-    deg = len(graphe[v])
-    degen[deg].append(v)
-    degrees[v] = deg
-    if deg > deg_max:
-      deg_max = deg
+#in this function, we create a dictionary
+#where the key is the degree and the value is the nodes
+def get_degree_list(graphe):
+    """create degree distribution"""
+    
+    D={}
+    
+    #if the current degree hasnt been checked
+    #we create a new key under the current degree
+    #otherwise we append the new node into the list
+    for i in graphe.keys():
+        try:
+            D[len(graphe[i])].append(i)
 
-  while True:
-    i = 0
-    while i <= deg_max:
-      if len(degen[i]) != 0:
-        break
-      i += 1
+        except KeyError:
+            D[len(graphe[i])]=[i]
+    
+    #dictionary is sorted by key instead of value in ascending order
+    D=dict(sorted(D.items()))
+    
+    return D
+
+# -----------
+
+def Degenerescence(graphe,ordering=False,degeneracy=False):
+    """ ".
+    """
+    
+    subset=copy.deepcopy(graphe)
+    
+    #k is the ultimate degeneracy
+    k=0
+    
+    #denote L as the checked list
+    L=[]
+    
+    #denote output as the storage of vertices in 1-core to k-core
+    output={}
+
+    #degree distribution
+    # print("subset =",subset,"et son type =",type(subset))
+    D=get_degree_list(subset)
+    # print("D =",D,"et son type =",type(D))
+    
+    #initialize
+    for i in range(1,max(D.keys())+1):
+        output[i]=[]
+    # print("outpout =",output)
+    #we initialize the current degree i to 0
+    #because we want to keep track of 1-core to k-core
+    i=0
+
+    while D:
+                
+        #denote i as the minimum degree in the current graph
+        i=list(D.keys())[0]
+                        
+        #k denotes the degeneracy
+        k=max(k,i)
+        
+        #pick a random vertex with the minimum degree
+        v=D[i].pop(0)
+        # print("v iteration=",v)
+        #checked and removed
+        L.append(v)
+        del subset[v]
+        # print("subset iteration=",subset)
+        # subset.remove(v)
+        output[k].append(v)
+        # print("output iteration=",output)
+        
+        #update the degree list
+        D=get_degree_list(subset)   
+    
+    #start from -2 to 0
+    for ind in sorted(output.keys(),reverse=True)[1:]:
+
+        #remove empty k-core
+        if not output[ind+1]:
+            del output[ind+1]
+
+        #add vertices from high order core to low order core
+        else:
+            output[ind]+=output[ind+1]
+    
+    #output depends on the requirement
+    if ordering:
+        return L
+    elif degeneracy:
+        return k
     else:
-      break
-    v = degen[i].pop()
-    ordre.append(v)
-    ordre_set.add(v)
-    for w in graphe[v]:
-      if w not in ordre_set:
-        deg = degrees[w]
-        degen[deg].remove(w)
-        if deg > 0:
-          degrees[w] -= 1
-          degen[deg - 1].append(w)
+        return output
 
-  ordre.reverse()
-  return ordre
 
 G = {   
         1: [2,3,4],
         2: [1,3,4,5],
         3: [1,2,4],
-        4: [1,2,3,6],
+        4: [1,2,3,5,6],
         5: [2,4,7],
         6: [4],
         7: [5]
@@ -88,4 +148,6 @@ G3 = {
         4: [2]
     }
 print(version_ameliore(G))
+# test = Degenerescence(G)
+# print(test)
 # print(version_ameliore(G2))
